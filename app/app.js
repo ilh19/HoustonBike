4f44a24d634eea7d76ced9d5088fc9cf22c2c893
Irma Lam <irmalam19@tamu.edu>
2013-05-18 14:04:17 -0500

    Adding search functionality and image for search bar

--- app/app.js
+++ app/app.js
@@ -50,6 +50,51 @@ if (Meteor.isClient) {
       $(this).toggleClass('marked');
     });
 
+    // search box functionality
+    var geocoder = new google.maps.Geocoder();
+    $(function() {
+        $("#searchbox").autocomplete({    
+            source: function(request, response) {
+    
+            if (geocoder == null){
+                geocoder = new google.maps.Geocoder();
+            }
+            geocoder.geocode( {'address': request.term }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+    
+                var searchLoc = results[0].geometry.location;
+                var lat = results[0].geometry.location.lat();
+                var lng = results[0].geometry.location.lng();
+                var latlng = new google.maps.LatLng(lat, lng);
+                var bounds = results[0].geometry.bounds;
+    
+                geocoder.geocode({'latLng': latlng}, function(results1, status1) {
+                    if (status1 == google.maps.GeocoderStatus.OK) {
+                        if (results1[1]) {
+                        response($.map(results1, function(loc) {
+                            return {
+                                label  : loc.formatted_address,
+                                value  : loc.formatted_address,
+                                bounds   : loc.geometry.bounds
+                            }
+                        }));
+                      }
+                    }
+                  });
+                }
+            });
+            },
+            select: function(event,ui){
+                var pos = ui.item.position;
+                var lct = ui.item.locType;
+                var bounds = ui.item.bounds;
+        
+                if (bounds){
+                    map.fitBounds(bounds);
+                }
+            }
+        });
+    });   
   }; // end template
 
   var switchVisibility = function(layer, map) {
@@ -107,5 +152,4 @@ if (Meteor.isClient) {
       }]
     });
   };
-
 }
