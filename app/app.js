0821df1763aae1e47af870ed8b6924e8639dbe5d
Jorge Palacio <juliocombativo@gmail.com>
2013-05-18 13:24:41 -0500

    Added yelp results

--- app/app.js
+++ app/app.js
@@ -58,31 +58,87 @@ if (Meteor.isClient) {
     });
     rentalLayer.setMap(map);
 	
-	var switchVisibility = function(layer, map) {
+	var switchVisibility = function(layer, map, event) {
 		if(layer.getMap())
 			layer.setMap(null);
 		else
 			layer.setMap(map);
+		$(event).toggleClass('marked');
 	}
 	
 	$('#rentalLayer').click(function(event) {
-		switchVisibility(rentalLayer, map);
-		$mark = $(this).find('.mark');
-		$(this).toggleClass('marked');
+		switchVisibility(rentalLayer, map, this);
 	});
 	$('#cityBikeLayer').click(function(event) {
-		switchVisibility(cityBikeLayer, map);
-		$(this).toggleClass('marked');
+		switchVisibility(cityBikeLayer, map, this);
 	});
 	$('#googleBikeLayer').click(function(event) {
-		switchVisibility(googleBikeLayer, map);
-		$(this).toggleClass('marked');
+		switchVisibility(googleBikeLayer, map, this);
+	});
+	
+	 
+	var center = map.getCenter();
+	var yelpMarkers = [];
+	Meteor.call('queryYelp', 'bike shop', center.lat(), center.lng(), '10', function(result, response) {
+		if(response.statusCode == 200) {
+			var yelpResponse = EJSON.parse(response.content);
+			var yelpResults = yelpResponse.businesses;
+				
+			for(var i = 0; i < yelpResults.length; i++) {
+				var biz = yelpResults[i];
+					
+				var options = {
+					position : new google.maps.LatLng(biz.latitude, biz.longitude),
+					map : map,
+					title: biz.name
+				};
+
+				var yelpMarker = new google.maps.Marker(options);
+				yelpMarker.biz = biz;
+				yelpMarkers.push(yelpMarker);
+					
+				google.maps.event.addListener(yelpMarker, 'click', function(e) {
+					for(var j = 0; j < yelpMarkers.length; j++) {
+						var currentMarker = yelpMarkers[j];
+						
+						if(currentMarker.position.equals(e.latLng)) {
+							var infoWindow = new google.maps.InfoWindow({
+								content: Template.yelpInfo(yelpMarkers[j].biz) 
+							});
+						
+							infoWindow.open(map,yelpMarkers[j]);
+						}
+					}
+					console.log(yelpMarkers, e.latLng);
+				});
+			}
+		}
 	});
   };
 }
 
-// if (Meteor.isServer) {
+if (Meteor.isServer) {
 //   Meteor.startup(function() {
 //     // code to run on server at startup
 //   });
-// }
+	Meteor.methods({
+		'queryYelp' : function(term, lat, longitude, radius, callback) {
+			var url = 'http://api.yelp.com/business_review_search?ywsid=4z8j2En3Yc4AI-Whlsrejw';
+			url = url + '&term=' + term;
+			url = url + '&lat=' + lat;
+			url = url + '&long=' + longitude;
+			url = url + '&radius=' + radius;
+			url = url + '&limit=' + 10;
+			return Meteor.http.get(url, {
+				'data' : {
+					'term': term,
+					'lat' : lat,
+					'long' : longitude,
+					'radius' : radius,
+					'limit' : 10,
+					'ywsid': '4z8j2En3Yc4AI-Whlsrejw'
+				}
+			});
+		}
+   });
+}
