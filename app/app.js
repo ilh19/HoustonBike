363edfea5fdcb3c7ed0a9f5141552cde0f7d5cbd
Irma Lam <irmalam19@tamu.edu>
2013-05-18 11:01:24 -0500

    Added BCycle logo. Needs styling.

--- app/app.js
+++ app/app.js
@@ -61,7 +61,10 @@ if (Meteor.isClient) {
     // setting the description for bike rental
     google.maps.event.addListener(rentalLayer, 'click', function(e) {
       // Change the content of the InfoWindow
-      e.infoWindowHtml = "<a href=" + "http://houston.bcycle.com" + " target=\"_blank\">" + "BCycle" + "</a>" + ": " + e.row['name'].value;
+      // BCycle logo, link, location name
+      e.infoWindowHtml = "<img src=\"Houston_B-cycle_Logo.jpg\" alt=\"Houston BCycle Logo\">" + 
+                         "<a href=" + "http://houston.bcycle.com" + " target=\"_blank\">" + 
+                         "BCycle" + "</a>" + ": " + e.row['name'].value;
     });
     
 	var switchVisibility = function(layer, map) {
